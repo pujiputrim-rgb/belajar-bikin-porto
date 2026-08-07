c3f9cc6a1e1cfacb62f1953c26d816afb4e01afc
pujiputrim-rgb <pujiputrim@gmail.com>
2026-08-07 10:09:53 +0700

    porto siap

--- portofolio.sql
+++ portofolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Agu 2026 pada 19.20
+-- Waktu pembuatan: 07 Agu 2026 pada 05.09
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -41,8 +41,8 @@ CREATE TABLE `blog` (
 --
 
 INSERT INTO `blog` (`id`, `image`, `title`, `subtitle`, `created_at`, `updated_at`) VALUES
-(1, '6a74b00774f9f_Logo_SMK50.png', 'Multimedia', 'SMK Negeri 50 Jakarta (2019-2022)', '2026-08-04 07:44:10', '2026-08-06 16:07:55'),
-(2, '6a74b111d41e2_Universitas-Terbuka.webp', 'S1 Ilmu Komunikasi', 'Universitas Terbuka (2023-Sekarang)', '2026-08-04 07:59:10', '2026-08-06 16:06:41');
+(4, '6a7523dfd1215_Logo_Universitas_Terbuka.svg.webp', 'S1 Ilmu Komunikasi', 'Universitas Terbuka (2023 – Sekarang)', '2026-08-07 00:16:31', NULL),
+(5, '6a752464d0789_logo-smk-50.png', 'Multimedia', 'SMK Negeri 50 Jakarta (2019 – 2022)', '2026-08-07 00:17:09', '2026-08-07 00:18:44');
 
 -- --------------------------------------------------------
 
@@ -108,9 +108,12 @@ CREATE TABLE `projects` (
 --
 
 INSERT INTO `projects` (`id`, `image`, `title`, `subtitle`, `button_link`, `created_at`, `updated_at`) VALUES
-(1, '6a71681dab118_desktop-green-pink.jpg', 'bbbbbbbbb', 'ccccccccc', 'https://youtu.be/lEXBxijQREo?si=CIwhMaZu-KW3xrbL', '2026-08-04 04:18:31', '2026-08-04 04:18:37'),
-(2, '6a716c7eac77f_desktop-polkadot.jpg', 'sssssss', 'ddddd', 'https://youtu.be/lEXBxijQREo?si=CIwhMaZu-KW3xrbL', '2026-08-04 04:37:18', NULL),
-(3, '6a7195eaa86a7_orang-ijo.jpg', 'aaaaaa', 'bbbbb', 'https://youtu.be/lEXBxijQREo?si=CIwhMaZu-KW3xrbL', '2026-08-04 04:46:53', '2026-08-04 07:34:02');
+(1, '6a7531c68701a_sosmed.png', 'Creav Studio', 'Social Media Specialist & Editor', 'https://youtu.be/lEXBxijQREo?si=CIwhMaZu-KW3xrbL', '2026-08-04 04:18:31', '2026-08-07 01:15:50'),
+(2, '6a75302e79f0a_project-card-twt.png', 'Social Media Card-Biodata Branding Design', 'Pembuatan Custom Biodata Card', 'https://youtu.be/lEXBxijQREo?si=CIwhMaZu-KW3xrbL', '2026-08-04 04:37:18', '2026-08-07 01:09:02'),
+(3, '6a752e802071c_project-ava-twt.png', 'Profile Social Media Branding Design', 'Pembuatan Custom Header & Avatar/Icon Profile', 'https://youtu.be/lEXBxijQREo?si=CIwhMaZu-KW3xrbL', '2026-08-04 04:46:53', '2026-08-07 01:01:52'),
+(5, '6a7534a89e0dc_Barista.png', 'Pelatihan Barista', 'Membuat Latte Art, Manual Brew, Teknik Espresso', NULL, '2026-08-07 01:28:08', NULL),
+(6, '6a7534e5595c4_sensus.png', 'REGSOSEK', 'Badan Pusar Statistik (BPS)', NULL, '2026-08-07 01:29:09', NULL),
+(7, '6a75351430880_pemilu.png', 'TPS Pemilu', 'Petugas KPPS', NULL, '2026-08-07 01:29:56', NULL);
 
 -- --------------------------------------------------------
 
@@ -134,10 +137,10 @@ CREATE TABLE `resume` (
 --
 
 INSERT INTO `resume` (`id`, `title`, `year_start`, `year_end`, `subtitle`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Social Media Specialist (Magang)', '2021', '2021', 'CREAV Studio', 'Pengelolaan terhadap pembuatan strategi konten, editing konten, engagement audiens, dan respon pesan.', '2026-08-01 07:32:45', '2026-08-06 15:22:56'),
-(2, 'Petugas REGSOSEK', '2022', '2022', 'Badan Pusat Statistik (BPS)', 'Sensus penduduk & ekonomi, wawancara warga, geotagging lokasi rumah tangga & verifikasi data.', '2026-08-01 08:06:38', '2026-08-06 15:24:56'),
-(3, 'Petugas TPS Pemilu', '2024', '2024', 'Pemilu Presiden 2024', 'Pencatatan dan validasi data suara serta digitalisasi hasil perhitungan via SIREKAP.', '2026-08-05 14:03:32', '2026-08-06 15:16:55'),
-(4, 'Pelatihan Barista', '2025', '2025', 'PPKD Jakarta Timur', 'Penerapan Service Excellence, teknik espresso & manual brew, serta manajemen inventaris bahan.', '2026-08-05 14:04:26', '2026-08-06 15:05:48');
+(1, 'Social Media Specialist (Magang)', '2021', '2021', 'CREAV Studio', 'Pengelolaan engagement audiens, respon pesan, dan pembuatan strategi konten.', '2026-08-01 07:32:45', '2026-08-07 00:23:48'),
+(2, 'Petugas REGSOSEK', '2022', '2022', 'Badan Pusat Statistik (BPS)', 'Wawancara warga, geotagging lokasi rumah tangga, dan verifikasi data lapangan.', '2026-08-01 08:06:38', '2026-08-07 00:23:02'),
+(3, 'Petugas TPS Pemilu', '2024', '2024', 'Pemilu Presiden 2024', 'Pencatatan dan validasi data suara serta digitalisasi hasil perhitungan via SIREKAP.', '2026-08-05 14:03:32', '2026-08-07 00:21:51'),
+(4, 'Pelatihan Barista', '2025', '2025', 'PPKD Jakarta Timur', 'Penerapan Service Excellence, teknik espresso & manual brew, serta manajemen inventaris.', '2026-08-05 14:04:26', '2026-08-07 00:21:04');
 
 -- --------------------------------------------------------
 
@@ -151,7 +154,7 @@ CREATE TABLE `settings` (
   `email` varchar(50) NOT NULL,
   `phone` varchar(15) NOT NULL,
   `address` text NOT NULL,
-  `description` varchar(300) DEFAULT NULL,
+  `description` varchar(200) DEFAULT NULL,
   `ig` varchar(50) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
@@ -162,7 +165,7 @@ CREATE TABLE `settings` (
 --
 
 INSERT INTO `settings` (`id`, `website_name`, `email`, `phone`, `address`, `description`, `ig`, `created_at`, `updated_at`) VALUES
-(1, 'puji putri meidi', 'pujiputrim@gmail.com', '087730711039', 'Jalan Malaka III, Kel. Malaka Sari, Kec. Duren Sawit, Jakarta Timur 13460', 'Saya merupakan seorang yang pada saat ini bergabung sebagai Anggota Kelas Website Programming PPKD Jakarta Pusat. Belajar mengenai HTML, CSS+, Javascript, Laravel, PHP.  Mengeksplor mengenai cara membuat website dinamis dan dapat diakses.', 'www.linkedin.com/in/pujiiputrim', '2026-07-30 02:51:40', '2026-08-06 14:16:56');
+(1, 'puji putri meidiani', 'pujiputrim@gmail.com', '087730711039', 'Jalan Malaka III, Kel. Malaka Sari, Kec. Duren Sawit, Jakarta Timur 13460', 'Mahasiswa Ilmu Komunikasi yang sedang mendalami Web Development (HTML, CSS, JS, PHP, Laravel) dan pengelolaan media digital.', 'https://www.linkedin.com/in/pujiiputrim', '2026-07-30 02:51:40', '2026-08-07 02:04:48');
 
 -- --------------------------------------------------------
 
@@ -183,14 +186,14 @@ CREATE TABLE `skills` (
 --
 
 INSERT INTO `skills` (`id`, `name`, `progress`, `created_at`, `updated_at`) VALUES
-(1, 'Google Workspace & MS Office', 80, '2026-08-03 06:53:24', '2026-08-06 14:50:52'),
-(2, 'Service Excellence', 85, '2026-08-03 06:54:32', '2026-08-06 14:50:21'),
-(3, 'Data Entry & Administrasi', 80, '2026-08-03 06:54:48', '2026-08-06 14:49:59'),
-(5, 'Public Speaking', 85, '2026-08-03 07:21:17', '2026-08-06 14:41:56'),
-(6, 'HTML5 & CSS3', 80, '2026-08-05 13:44:56', '2026-08-06 14:49:21'),
-(7, 'Problem Solving', 80, '2026-08-06 14:51:13', NULL),
-(8, 'Multimedia Design', 77, '2026-08-06 14:51:35', NULL),
-(9, 'Time Management & Teamwork', 85, '2026-08-06 14:52:08', NULL);
+(1, 'Time Management & Teamwork', 87, '2026-08-03 06:53:24', '2026-08-07 00:39:23'),
+(2, 'Multimedia Design', 80, '2026-08-03 06:54:32', '2026-08-07 00:38:51'),
+(3, 'Problem Solving', 80, '2026-08-03 06:54:48', '2026-08-07 00:38:25'),
+(5, 'Google Workspace & MS Office', 85, '2026-08-03 07:21:17', '2026-08-07 00:38:05'),
+(6, 'Service Excellence', 85, '2026-08-05 13:44:56', '2026-08-07 00:37:35'),
+(7, 'Data Entry & Administrasi', 80, '2026-08-07 00:35:28', '2026-08-07 00:37:19'),
+(8, 'Public Speaking', 90, '2026-08-07 00:35:33', '2026-08-07 00:36:40'),
+(9, 'HTML5 & CSS3', 75, '2026-08-07 00:35:42', '2026-08-07 00:36:15');
 
 -- --------------------------------------------------------
 
@@ -218,8 +221,8 @@ CREATE TABLE `sliders` (
 --
 
 INSERT INTO `sliders` (`id`, `title`, `subtitle`, `description`, `button1_text`, `button1_link`, `button2_text`, `button2_link`, `image`, `is_active`, `created_at`, `updated_at`) VALUES
-(14, 'Komunikasi & Teknologi', 'Web Developer & Content Specialist', 'Berfokus pada pengembangan website yang responsif serta pengelolaan komunikasi digital yang efektif.', 'IG', 'https://www.instagram.com/pujiptrm/?utm_source=ig_', 'IN', 'https://www.linkedin.com/in/pujiiputrim', '6a6d931f53046_giphy-judyhopps.gif', 1, '2026-08-01 06:33:03', '2026-08-06 15:39:53'),
-(16, 'Puji Putri Meidiani', 'Web Programming', 'Selamat datang! Ini adalah tempat saya membagikan karya, proyek, dan perjalanan di dunia web development.', 'IG', 'https://www.instagram.com/pujiptrm/?utm_source=ig_', 'IN', 'https://www.linkedin.com/in/pujiiputrim', '6a6d947d26b91_nick-wilde.gif', 1, '2026-08-01 06:38:53', '2026-08-06 14:24:31');
+(14, 'Komunikasi & Teknologi', 'Web Programming, Service Excellence, Content Speci', 'Menggabungkan keahlian komunikasi publik dan pemrograman web untuk menciptakan solusi digital fungsional.', 'IG', 'https://www.instagram.com/pujiptrm/?utm_source=ig_', 'IN', 'https://www.linkedin.com/in/pujiiputrim', '6a7536ba54e01_judy-nick.gif', 1, '2026-08-01 06:33:03', '2026-08-07 01:36:58'),
+(16, 'PujiPutri Meidiani', 'Web Developer', 'Selamat datang! Ini adalah tempat saya membagikan karya, proyek, dan perjalanan di dunia web development.', 'IN', 'https://www.linkedin.com/in/pujiiputrim', 'IG', 'https://www.instagram.com/pujiptrm/?utm_source=ig_', '6a7537ce3b18d_profile-depan.png', 1, '2026-08-01 06:38:53', '2026-08-07 02:34:02');
 
 -- --------------------------------------------------------
 
@@ -311,7 +314,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `contacts`
@@ -329,7 +332,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT untuk tabel `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `resume`
